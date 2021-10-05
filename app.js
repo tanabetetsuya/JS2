18e6b29d26f6828e7a2bf02cef6fcfb5b4980276
田辺　哲也 <tanabetetsuya@tanabetuyanoMBP.elecom>
2021-10-06 07:42:25 +0900

    [Update] 条件式の記載

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-let int1 = 1;
-alert(int1);
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんよりりんごの方が高い');
+}else if(orange == apple){
+  alert('みかんとりんごは同じ値段');
+}else{
+  alert('みかんの方がりんごより高い');
+}
